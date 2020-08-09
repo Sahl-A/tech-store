dbdd21b87042f0393cbd7881935fd71d3e5733a5
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-08-09 13:37:23 +0200

    4: Add About & Error pages. Add Hero component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className='section about-section'>
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error in est vel illo, et ipsum dicta voluptas eveniet harum, laboriosam suscipit fugiat voluptatem autem atque quidem inventore nihil? Totam, placeat!
+      </p>
+    </section>
+  )
 }
