9fb6af5c5e6fc59a9e05d12e50e780e10577e8c0
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-08-09 08:59:19 +0200

    2: Add routing for all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+// Router
+import { Switch, Route } from "react-router";
+// Pages
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
 
 function App() {
   return (
-    <h1>Hello from app component</h1>
+    <>
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id">
+          <ProductDetails />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
