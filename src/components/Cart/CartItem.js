f96e5eb4816ac83bd72e9cc21ee2175a0e2c706a
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-08-10 14:19:40 +0200

    14: Complete Cart page and CartItem component. Without any function

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,44 @@
 import React from "react";
+import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 
-export default function CartItem() {
-  return <h1>hello form cart item</h1>;
+export default function CartItem({ id, price, title, image, amount }) {
+  return (
+    <article className="cart-item">
+      <img src={image} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h5>${price}</h5>
+        <button
+          type="button"
+          className="cart-btn remove-btn"
+          onClick={() => {
+            console.log("item removed");
+          }}
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => {
+            console.log("Amount Increased");
+          }}
+        >
+          <FaAngleUp />
+        </button>
+        <p className="item-amount">{amount}</p>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => {
+            console.log("Amount Decreased");
+          }}
+        >
+          <FaAngleDown />
+        </button>
+      </div>
+    </article>
+  );
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,9 +3,29 @@ import { CartContext } from "../context/cartContext";
 // import { UserContext } from "../context/userContext";
 import EmptyCart from "../components/Cart/EmptyCart";
 import CartItem from "../components/Cart/CartItem";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
+  // Will be removed later, to check wheather user is authenticated
+  const auth = false;
   const { cart, total } = React.useContext(CartContext);
   if (!cart.length) return <EmptyCart />;
-  return <h1>Hello from cart page</h1>
+  return (
+    <section className="cart-items section">
+      <h2>Your cart</h2>
+      {cart.map((item) => (
+        <CartItem key={item.id} {...item} />
+      ))}
+      <h2>Total: ${total}</h2>
+      {auth ? (
+        <Link to="/checkout" className="btn-primary btn btn-block">
+          checkout
+        </Link>
+      ) : (
+        <Link to="/login" className="btn-primary btn btn-block">
+          login
+        </Link>
+      )}
+    </section>
+  );
 }
